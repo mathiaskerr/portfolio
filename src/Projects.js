0eea66b82c213b4c47153978e687043063a0ef24
mathiaskerr <114953633+mathiaskerr@users.noreply.github.com>
2023-03-27 22:22:30 +0100

    /

--- src/Projects.js
+++ src/Projects.js
@@ -13,30 +13,18 @@ const Projects = () => {
         <h2>Python Solo Project</h2>
         <p>project info/brief</p>
         <p>learning points</p>
-
-        <div>
-        <iframe src={PythonSrc} frameborder="0" width="100%" height="100%" allowfullscreen allow="autoplay">
-        </iframe>
-        </div>
-
-   
-
         <p>view project here</p>
-
-
-        <a href="https://github.com/mathiaskerr/PROJECT"><button> GitHub</button></a>
+        <a href="https://github.com/mathiaskerr/PROJECT"><button> GitHub</button></a> 
+        <iframe src={PythonSrc} frameborder="0" width="560" height="315" allowfullscreen/>
     </div>
 
     <div className='react'>
         <h2>React Group Project</h2>
         <p>project info/brief</p>
         <p>learning points</p>
-
-        <iframe src={ReactSrc} frameborder="0" width="100%" height="100%" allowfullscreen allow="autoplay"/>
-
-        
         <p>view project here</p>
         <a href="https://github.com/ElizaPeterson/Portals"><button> GitHub</button></a>
+        <iframe src={ReactSrc} frameborder="0" width="560" height="315" allowfullscreen/>
     </div>
     
     </div>
